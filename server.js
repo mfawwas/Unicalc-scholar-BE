39a1bbec0cdb5ca4090a507188ec2125373c49ce
ravatech3 <antidroidconcepts@gmail.com>
2026-04-01 15:46:03 +0100

    vercel update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://unicalc-scholar.netlify.app"],
+    origin: ["http://localhost:5173", "https://unicalc-server.vercel.app/"],
     credentials: true,
   })
 );
